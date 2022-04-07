928e18125069f7208a455621a79541030812c940
Cafe137 <xcafe137@gmail.com>
2022-04-07 15:48:01 +0200

    fix: make pathfinder stay in root directory

--- src/path.ts
+++ src/path.ts
@@ -2,6 +2,18 @@ import { existsSync } from 'fs'
 import { join, parse, sep } from 'path'
 
 export function resolvePath(path: string) {
+  return makePath(path)
+}
+
+export function makePath(path: string) {
+  return join(parse(findAnywhere('tray.png')).dir, path) // FIXME
+}
+
+export function canResolvePath(path: string) {
+  return existsSync(resolvePath(path))
+}
+
+function findAnywhere(path: string) {
   const origin = process.execPath
   const parts = origin.split(sep)
   while (parts.length) {
@@ -13,18 +25,4 @@ export function resolvePath(path: string) {
     parts.pop()
   }
   throw Error(`Path ${path} is not found`)
-}
-
-export function makePath(path: string) {
-  return join(parse(resolvePath('tray.png')).dir, path) // FIXME
-}
-
-export function canResolvePath(path: string) {
-  try {
-    resolvePath(path)
-
-    return true
-  } catch {
-    return false
-  }
 }
