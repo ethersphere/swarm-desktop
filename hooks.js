662e8659755075b75ae67b58f5efffa9a53a7b67
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2022-04-07 15:15:56 +0200

    build: download bee binary in forge hook (#44)

--- hooks.js
+++ hooks.js
@@ -1,8 +1,26 @@
-const { existsSync } = require('fs')
+const { existsSync, writeFileSync } = require('fs')
 const { copySync } = require('fs-extra')
 const { join } = require('path')
+const { platform } = require('os')
+const { default: fetch } = require('node-fetch')
+const { execSync } = require('child_process')
+
+const binaries = {
+  darwin: ['https://github.com/ethersphere/bee/releases/download/v1.5.1-rc1/bee-darwin-amd64', 'bee'],
+  linux: ['https://github.com/ethersphere/bee/releases/download/v1.5.1-rc1/bee-linux-amd64', 'bee'],
+  win32: ['https://github.com/ethersphere/bee/releases/download/v1.5.1-rc1/bee-windows-amd64.exe', 'bee.exe'],
+}
 
 module.exports = {
+  generateAssets: async () => {
+    const target = binaries[platform()]
+    if (!existsSync(target[1])) {
+      await downloadFile(target[0], target[1])
+      try {
+        execSync(`chmod +x ${target[1]}`)
+      } catch {}
+    }
+  },
   postPackage: (_, options) => {
     if (existsSync('bee')) {
       copySync('bee', join(options.outputPaths[0], 'bee'))
@@ -15,4 +33,10 @@ module.exports = {
     copySync('icon.png', join(options.outputPaths[0], 'icon.png'))
     copySync('static', join(options.outputPaths[0], 'static'))
   },
+}
+
+function downloadFile(url, target) {
+  return fetch(url)
+    .then(x => x.arrayBuffer())
+    .then(x => writeFileSync(target, Buffer.from(x)))
 }
