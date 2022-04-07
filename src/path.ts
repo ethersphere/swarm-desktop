b02143e99ddcf440e90eb9e8f40991978f64e6e7
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2022-04-07 15:16:53 +0200

    feat: add os agnostic pathfinder (#41)

--- src/path.ts
+++ src/path.ts
@@ -1,24 +1,16 @@
-import { app } from 'electron'
-import { platform } from 'os'
-import { join, parse, resolve } from 'path'
+import { existsSync } from 'fs'
+import { join, sep } from 'path'
 
 export function resolvePath(path: string) {
-  if (platform() === 'win32') {
-    if (process.execPath.includes('node_modules')) {
-      return join(process.execPath.split('node_modules')[0], path)
+  const origin = process.execPath
+  const parts = origin.split(sep)
+  while (parts.length) {
+    const currentPath = join(sep, ...parts, path)
+
+    if (existsSync(currentPath)) {
+      return currentPath
     }
-    const parsedPath = parse(process.execPath)
-    return join(parsedPath.dir, path)
+    parts.pop()
   }
-  if (process.execPath.includes('node_modules/electron/dist/Electron.app')) {
-    return resolve(path)
-  }
-  const appName = `${app.getName()}.app`
-  let execPath = process.execPath
-
-  if (execPath.includes(appName)) {
-    execPath = execPath.split(appName)[0]
-  }
-
-  return join(execPath, path)
+  throw Error(`Path ${path} is not found`)
 }
