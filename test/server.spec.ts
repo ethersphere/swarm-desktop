0da13e4203ebcb6926c20e3cd0eef5247af29299
Vojtech Simetka <vojtech@simetka.cz>
2022-04-10 18:48:07 +0200

    chore: remove unnecessary server spawning

--- test/server.spec.ts
+++ test/server.spec.ts
@@ -1,19 +1,7 @@
 import request from 'supertest'
-import type { Server } from 'http'
 import { createApp } from '../src/server'
 
 const app = createApp()
-let server: Server
-
-beforeAll(async () => {
-  server = await new Promise((resolve, _reject) => {
-    const server = createApp().listen(async () => resolve(server))
-  })
-})
-
-afterAll(async () => {
-  await new Promise(resolve => server.close(resolve))
-})
 
 describe('status', () => {
   it('should get status', async () => {
