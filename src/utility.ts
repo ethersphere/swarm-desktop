39bf2b421cece9e71795eda3360eb2c968dc6c1a
Adam Uhlíř <adam@uhlir.dev>
2022-06-20 11:35:39 +0200

    feat: sentry proxy (#112)

--- src/utility.ts
+++ src/utility.ts
@@ -1,5 +1,16 @@
+import { Readable } from 'stream'
+
 export async function wait(ms: number): Promise<void> {
   return new Promise(resolve => {
     setTimeout(resolve, ms)
   })
+}
+
+export async function bufferRequest(red: Readable): Promise<string> {
+  const arr = []
+  for await (const redElement of red) {
+    arr.push(redElement)
+  }
+
+  return Buffer.concat(arr).toString('utf8')
 }
