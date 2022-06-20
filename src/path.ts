43b231dc42e6d49d2915272fc869fea179129bb2
Cafe137 <xcafe137@gmail.com>
2022-06-21 00:29:09 +0200

    fix: change envPaths to Swarm Desktop

--- src/path.ts
+++ src/path.ts
@@ -2,7 +2,7 @@ import envPaths from 'env-paths'
 import { existsSync } from 'fs'
 import { join } from 'path'
 
-export const paths = envPaths('SwarmDesktop', { suffix: '' })
+export const paths = envPaths('Swarm Desktop', { suffix: '' })
 
 export function checkPath(path: string): boolean {
   return existsSync(getPath(path))
