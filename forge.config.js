7e5feca14c824da558785d7784a550313b5dd32b
Adam Uhlíř <adam@uhlir.dev>
2022-06-28 14:22:02 +0200

    feat: code signing (#137)

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,16 @@
 const path = require('path')
+const fs = require('fs')
 
 // Taken over from https://github.com/electron/fiddle/blob/main/forge.config.js
+
+if (process.env['WINDOWS_CODESIGN_FILE']) {
+  const certPath = path.join(__dirname, 'win-certificate.pfx')
+  const certExists = fs.existsSync(certPath)
+
+  if (certExists) {
+    process.env['WINDOWS_CODESIGN_FILE'] = certPath
+  }
+}
 
 const iconPath = path.resolve(__dirname, 'assets', 'icon.icns')
 
@@ -15,10 +25,12 @@ const config = {
       OriginalFilename: 'Swarm Desktop',
     },
     osxSign: {
+      identity: '3rd Party Mac Developer Application: Swarm Association (9J9SPHU9RP)',
       hardenedRuntime: true,
       'gatekeeper-assess': false,
       entitlements: 'assets/entitlements.plist',
       'entitlements-inherit': 'assets/entitlements.plist',
+      'signature-flags': 'library',
     },
   },
   electronInstallerDebian: {
@@ -29,6 +41,8 @@ const config = {
       name: '@electron-forge/maker-squirrel',
       config: {
         name: 'swarm-desktop',
+        certificateFile: process.env['WINDOWS_CODESIGN_FILE'],
+        certificatePassword: process.env['WINDOWS_CODESIGN_PASSWORD'],
       },
     },
     {
@@ -61,5 +75,30 @@ const config = {
     },
   ],
 }
+
+function notarizeMaybe() {
+  if (process.platform !== 'darwin') {
+    return
+  }
+
+  if (!process.env.CI) {
+    console.log(`Not in CI, skipping notarization`)
+    return
+  }
+
+  if (!process.env.APPLE_ID || !process.env.APPLE_ID_PASSWORD) {
+    console.warn('Should be notarizing, but environment variables APPLE_ID or APPLE_ID_PASSWORD are missing!')
+    return
+  }
+
+  config.packagerConfig.osxNotarize = {
+    appBundleId: 'org.ethswarm.swarmDesktop',
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    ascProvider: '9J9SPHU9RP',
+  }
+}
+
+notarizeMaybe()
 
 module.exports = config
