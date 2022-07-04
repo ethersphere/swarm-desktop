6ff2f8ce54a8f50a554a007e3d5e8c912f70a73a
Adam Uhlíř <adam@uhlir.dev>
2022-07-04 17:28:55 +0200

    chore: update splash screen (#190)

--- src/splash.ts
+++ src/splash.ts
@@ -8,7 +8,7 @@ export async function initSplash(): Promise<() => void> {
       const splashImage = path.resolve(__dirname, '..', '..', '..', 'assets', 'splash.html')
       logger.info(`Serving splash screen from path ${splashImage}`)
 
-      const splash = new BrowserWindow({ width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true })
+      const splash = new BrowserWindow({ width: 800, height: 600, frame: false, alwaysOnTop: true })
       splash.loadURL(`file://${splashImage}`).catch(reject)
 
       resolve(() => splash.hide())
