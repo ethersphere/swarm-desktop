76406e58ef99effe4750d65a112dc4cec58f4432
Vojtech Simetka <vojtech@simetka.cz>
2022-08-03 16:09:25 +0200

    feat: provide isBeeDesktop to the bee-dashboard component (#243)

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,15 +4,12 @@ import Installer from './installer/Installer'
 import Dashboard from '@ethersphere/bee-dashboard'
 import ReactDOM from 'react-dom'
 
-import '@ethersphere/bee-dashboard/lib/assets/dashboard-logo.svg'
-import '@ethersphere/bee-dashboard/lib/assets/desktop-logo.svg'
-
 const container = document.getElementById('root')
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard isBeeDesktop={true} />} />
         <Route path="/installer" element={<Installer />} />
       </Routes>
     </BrowserRouter>
