44c6dc38361bf965f0209f8243892c4908161047
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2023-01-20 18:10:55 +0100

    fix: upgrade bee to apply hotfix (#316)

--- src/downloader.ts
+++ src/downloader.ts
@@ -41,7 +41,7 @@ export async function runDownloader(force = false): Promise<void> {
   }
   await ensureDir(paths.data)
   await ensureAsset(
-    `https://github.com/ethersphere/bee/releases/download/v1.11.0/bee-${platformString}-${archString}${suffixString}`,
+    `https://github.com/ethersphere/bee/releases/download/v1.11.1/bee-${platformString}-${archString}${suffixString}`,
     `bee${suffixString}`,
     { chmod: process.platform !== 'win32', force },
   )
