9fbe24a10e7afaf57d37ed4afeae0987f67dd09b
Cafe137 <xcafe137@gmail.com>
2023-08-12 01:50:53 +0200

    build: add open

--- ui/src/utils/sentry.ts
+++
@@ -1,9 +0,0 @@
-import { getHost, sendRequest } from './net'
-
-async function getBeeDesktopLogs(): Promise<string> {
-  return await sendRequest<string>(`${getHost()}/logs/bee-desktop`, 'GET', undefined, 'string')
-}
-
-async function getBeeLogs(): Promise<string> {
-  return await sendRequest<string>(`${getHost()}/logs/bee`, 'GET', undefined, 'string')
-}
