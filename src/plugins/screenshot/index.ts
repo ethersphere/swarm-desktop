0570c1bdcf475f35556e4022f75852e477cf7ee2
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-11-05 12:47:55 +0100

    update: made to function name

--- src/plugins/screenshot/index.ts
+++ src/plugins/screenshot/index.ts
@@ -1,7 +1,2 @@
-/**
- * @dev This function takes the screeshot of window
- * and uploads it to the Swarm Network
- */
-export function takeScreenshot(): void {
-
-}
+export { screenCaptureWindow } from './screenCaptureWindow/capture'
+export { runScreenshotImpl } from './screenshotMain'
