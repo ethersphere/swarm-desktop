0bd85848341dc3177f621de9dbf8192759325ee3
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-11-05 12:46:27 +0100

    update: add errorHandler to utils

--- src/plugins/screenshot/utils/index.ts
+++ src/plugins/screenshot/utils/index.ts
@@ -1,4 +1,46 @@
-import { screen } from 'electron'
+import { dialog, screen } from 'electron'
+
+import { logger } from '../../..//logger'
+
+export type CreateElementArg<T extends keyof HTMLElementTagNameMap> = {
+  tagName: T
+  options?: {
+    type?: HTMLInputElement['type']
+  }
+  id: string
+  textContent?: string
+  classes?: string[]
+  attributes?: {
+    [key: string]: string
+  }
+}
+
+export function createElement<T extends keyof HTMLElementTagNameMap>(
+  args: CreateElementArg<T>,
+): HTMLElementTagNameMap[T] {
+  const elem = document.createElement(args.tagName)
+
+  if (args.id) elem.id = args.id
+
+  if (args.textContent) elem.textContent = args.textContent
+  args.classes?.forEach(clsName => elem.classList.add(clsName))
+
+  Object.entries(args.attributes || {}).forEach(([k, v]) => {
+    elem.setAttribute(k, v)
+  })
+
+  return elem as HTMLElementTagNameMap[T]
+}
+
+export function errorHandler(e: Error | string) {
+  if (typeof e !== 'string') {
+    e = e.message
+  }
+
+  logger.error(e)
+  dialog.showErrorBox('There was an error in Swarm Desktop', e)
+}
+
 
 /**
  *
