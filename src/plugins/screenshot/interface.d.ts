0e3f8ed226a91bb7a7a47e1fd6f0808c2a73af36
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-11-07 15:33:39 +0100

    update: add onImageDataURL

--- src/plugins/screenshot/interface.d.ts
+++ src/plugins/screenshot/interface.d.ts
@@ -1,9 +1,12 @@
 import type { CreateElementArg } from './utils'
 
+export type OnImageDataURL = (imgData: string) => void
+
 export interface IElectronAPI {
   setChildWindowOpacity: (num: number) => void
   takeScreenshot: () => Promise<void>
   setCaptureWindowOpacity: () => void
+  onImageDataURL: OnImageDataURL
   createElement: <T extends keyof HTMLElementTagNameMap>(args: CreateElementArg<T>) => HTMLElementTagNameMap[T]
 }
 
