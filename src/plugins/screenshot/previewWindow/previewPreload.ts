e71b4c6f5886706b6ede6eb7f1932b23bf441c99
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-11-07 16:55:40 +0100

    update: add openCropWindow ipc message

--- src/plugins/screenshot/previewWindow/previewPreload.ts
+++ src/plugins/screenshot/previewWindow/previewPreload.ts
@@ -3,4 +3,5 @@ import type { OnImageDataURL } from '../interface'
 
 contextBridge.exposeInMainWorld('electron', {
   onImageDataURL: (cb: OnImageDataURL) => ipcRenderer.on('image-data-url', (_, dataURL) => cb(dataURL)),
+  openCropWindow: (imgSrc: string) => ipcRenderer.send('open-crop-window', imgSrc),
 })
