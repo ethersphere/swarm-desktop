139b234a43281678aa93604a598c85e4f7ce3b7f
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-11-07 23:31:57 +0100

    update: add onCroppedImage ipc message

--- src/plugins/screenshot/previewWindow/previewPreload.ts
+++ src/plugins/screenshot/previewWindow/previewPreload.ts
@@ -4,4 +4,6 @@ import type { OnImageDataURL } from '../interface'
 contextBridge.exposeInMainWorld('electron', {
   onImageDataURL: (cb: OnImageDataURL) => ipcRenderer.on('image-data-url', (_, dataURL) => cb(dataURL)),
   openCropWindow: (imgSrc: string) => ipcRenderer.send('open-crop-window', imgSrc),
+  onCroppedImage: (cb: (imgDataURL: string) => void) =>
+    ipcRenderer.on('update-with-cropped-image', (_evnt, imgDataURL) => cb(imgDataURL)),
 })
