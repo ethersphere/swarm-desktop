1508e8501101ff7574c48a88c46af4e350c861f0
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-11-07 15:25:58 +0100

    update: add onImageDataURL ipc message

--- src/plugins/screenshot/previewWindow/previewPreload.ts
+++ src/plugins/screenshot/previewWindow/previewPreload.ts
@@ -1,1 +1,6 @@
+import { contextBridge, ipcRenderer } from 'electron'
+import type { OnImageDataURL } from '../interface'
 
+contextBridge.exposeInMainWorld('electron', {
+  onImageDataURL: (cb: OnImageDataURL) => ipcRenderer.on('image-data-url', (_, dataURL) => cb(dataURL)),
+})
