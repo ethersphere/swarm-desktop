2a307dd1674088e4d28fdbe9183341a8b36881fb
Ferenc Sárai <sarai.ferenc@gmail.com>
2026-02-12 12:27:38 +0100

    fix: update downloader to use bee version 2.7.0 spdv-944

--- src/downloader.ts
+++ src/downloader.ts
@@ -41,7 +41,7 @@ export async function runDownloader(force = false): Promise<void> {
   }
   await ensureDir(paths.data)
   await ensureAsset(
-    `https://github.com/ethersphere/bee/releases/download/v2.6.0/bee-${platformString}-${archString}${suffixString}`,
+    `https://github.com/ethersphere/bee/releases/download/v2.7.0/bee-${platformString}-${archString}${suffixString}`,
     `bee${suffixString}`,
     { chmod: process.platform !== 'win32', force },
   )
